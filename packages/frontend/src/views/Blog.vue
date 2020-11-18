54b9a691ee12f5fc0c2daa174f9e8a5482aada8d
Nikolaj <Nikodyring@gmail.com>
2020-11-19 00:19:22 +0100

    add(frontend): add blog view

---
+++ packages/frontend/src/views/Blog.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+BlogPost
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import BlogPost from '@/components/BlogPost.vue'
+
+export default defineComponent({
+  name: 'Blog',
+  components: {
+    BlogPost
+  }
+})
+</script>
+
+<style lang="stylus">
+.blog
+  background-color #141617
+</style>
