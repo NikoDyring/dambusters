6aea3aa5a0b4911d5986920b6efc193c873764cd
Nikolaj <Nikodyring@gmail.com>
2020-11-19 00:18:42 +0100

    add(frontend): Add blogpost template

---
+++ packages/frontend/src/components/BlogPost.vue
@@ -0,0 +1,15 @@
+<template>
+  <main class="mt-10">
+    <Header />
+    <Body />
+  </main>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Header from '@/components/blog/post/Header.vue'
+import Body from '@/components/blog/post/Body.vue'
+export default defineComponent({
+  name: 'BlogPost',
+  components: { Header, Body }
+})
+</script>
---
+++ packages/frontend/src/components/blog/post/Header.vue
@@ -0,0 +1,28 @@
+<template>
+<div class="mb-4 md:mb-0 w-full max-w-screen-md mx-auto relative" style="height: 24em;">
+  <div class="absolute left-0 bottom-0 w-full h-full z-10"
+    style="background-image: linear-gradient(180deg,transparent,rgba(0,0,0,.7));"></div>
+  <img src="https://news-cdn.softpedia.com/images/news2/WOW-Warlords-of-Draenor-Friend-Finder-Gets-New-Premade-Group-Options-455122-2.jpg" class="absolute left-0 top-0 w-full h-full z-0 object-cover" />
+    <div class="p-4 absolute bottom-0 left-0 z-20">
+      <a href="#"
+        class="px-4 py-1 bg-black text-gray-200 inline-flex items-center justify-center mb-2">News</a>
+      <h2 class="text-4xl font-semibold text-gray-100 leading-tight">
+        Having Two Raid Teams
+      </h2>
+      <div class="flex mt-3">
+        <img src="https://render-eu.worldofwarcraft.com/character/kazzak/124/173768316-avatar.jpg"
+          class="h-10 w-10 rounded-full mr-2 object-cover" />
+        <div>
+          <p class="font-semibold text-gray-200 text-sm"> Bigpyjamas </p>
+          <p class="font-semibold text-gray-400 text-xs"> 17 Nov </p>
+        </div>
+    </div>
+  </div>
+</div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'Header'
+})
+</script>
